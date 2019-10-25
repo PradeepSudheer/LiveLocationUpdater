0f1db212ea1bb54a5b82ddcd155e7b9d03b1941f
PradeepSudheer <pradeepsudheer944@gmail.com>
2019-10-25 22:29:32 +0530

    Update TestApplication.java

--- app/src/main/java/com/example/maptrack/TestApplication.java
+++ app/src/main/java/com/example/maptrack/TestApplication.java
@@ -5,8 +5,8 @@ import android.app.Application;
 import com.backendless.Backendless;
 
 public class TestApplication extends Application {
-    public static final String APPLICATION_ID  = "FBB3E2B8-0D11-09CF-FFD2-23758F7BEB00";
-    public static final String API_KEY = "FCFA0100-F819-1CC8-FF29-0F2B82266B00";
+    public static final String APPLICATION_ID  = "YOUR_APPLICATION_ID";
+    public static final String API_KEY = "YOUR_API_KEY";
     public static final String SERVER_URL = "https://api.backendless.com";
 
     @Override
